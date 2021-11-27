b08c56a88faab69de93a44e814b8d0e957629545
studenovAlex <alexstudenov44@gmail.com>
2021-11-27 15:33:14 +0200

    card for user

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,10 +16,14 @@
         @foreach($users as $user)
         <tr>
             <th scope="row">{{ $user->id }}</th>
-            <td>{{ $user->name }}</td>
-            <td>{{ $user->email }}</td>
             <td>
-                <a href="{{ route('users.edit', $user) }}" class="btn btn-warning">Edit</a>
+                <a href="{{ route('users.show', $user) }}"> {{ $user->name }} </a>
+            </td>
+            <td>
+                <a href="{{ route('users.show', $user) }}"> {{ $user->email }} </a>
+            </td>
+            <td>
+                <a type="button" href="{{ route('users.edit', $user) }}" class="btn btn-warning">Edit</a>
             </td>
         </tr>
         @endforeach
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,7 +1,17 @@
 @extends('layout')
 
-@section('title', 'Users')
+@section('title', 'User' . $user->name)
 
 @section('content')
-
+    <a href="{{ route('users.index') }}" class="btn btn-secondary">Back to users</a>
+    <div class="card mt-3" style="width: 18rem;">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Id: {{$user->id}}</li>
+            <li class="list-group-item">Name: {{$user->name}}</li>
+            <li class="list-group-item">Email: {{$user->email}}</li>
+            <li class="list-group-item">Created: {{$user->created_at->format('d/m/y H:i:s')}}</li>
+            <li class="list-group-item">Updated: {{$user->updated_at->format('d/m/y H:i:s')}}</li>
+        </ul>
+    </div>
+    <a type="button" href="{{ route('users.edit', $user) }}" class="btn btn-warning mt-3">Edit</a>
 @endsection
